f3d6432ef34cf087e06ce5d840278412c6764d99
vijila <vijilavk46@gmail.com>
2025-09-18 15:55:27 +0530

    feat:fetch values to the child table of manifest doc

--- snelex/snelex/doctype/manifest/manifest.js
+++ snelex/snelex/doctype/manifest/manifest.js
@@ -3,7 +3,7 @@
 
 
 frappe.ui.form.on("Manifest",{
-    refresh(frm){
+    refresh:function(frm){
         frm.set_query("truck",function(){
             return{
                 filters:{
@@ -12,5 +12,31 @@ frappe.ui.form.on("Manifest",{
             }
                 
         })
+    },
+    get_consignment_details:function(frm){
+        frappe.call({
+            method:'snelex.snelex.doctype.manifest.manifest.get_consignment_details',
+            args:{
+             manifest_date:frm.doc.manifest_date,
+             location:frm.doc.location
+
+            },
+            callback:function(r){
+                if (r.message){
+                    frm.clear_table("consignment_details")
+                    r.message.forEach(function(row)
+                {
+                    let child=frm.add_child("consignment_details");
+                    child.consignment_number=row.name;
+                    child.consignment_date=row.consignment_date;
+                    child.consignee=row.consignee_customer;
+                    child.shipper=row.shipper;
+                    child.remarks=row.remarks;
+                });
+                frm.refresh_field("consignment_details")
+
+                }
+            }
+        });
     }
 });
--- snelex/snelex/doctype/manifest/manifest.py
+++ snelex/snelex/doctype/manifest/manifest.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2025, sammish and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Manifest(Document):
 	pass
+@frappe.whitelist()
+def get_consignment_details(manifest_date,location):
+	# "arguments are from manifest doctype"
+	if manifest_date and location:
+		consignment=frappe.get_all("Consignment Note",
+		filters={
+			"consignment_date":manifest_date,
+			"consignment_to":location
+
+		},
+		fields=["name","consignment_date","consignee_customer","shipper","remarks"]
+		)
+		if consignment:
+			return consignment
+
+
